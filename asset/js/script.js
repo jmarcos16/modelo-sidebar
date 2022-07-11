9b781d01f213c7bdfe54817e7d564e6ade597faf
Zé Marcos <jose.ferreira@xpm.local>
2022-07-11 12:08:02 -0300

    mudanças de layout

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,10 @@
 let sidebar = document.querySelector(".sidebar");
 let sidebarBtn = document.querySelector(".bx-menu");
+let sidebarBtnMobile = document.querySelector("#close-menu");
+
+sidebarBtnMobile.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
 
 sidebarBtn.addEventListener("click", () => {
   sidebar.classList.toggle("close");
@@ -12,4 +17,9 @@ for (var i = 0; i < arrow.length; i++) {
     arrowParent.classList.toggle("showMenu");
     console.log(arrowParent);
   });
+}
+
+let width = screen.width;
+if (width < 600) {
+  sidebar.classList.remove("close");
 }
